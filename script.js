2e066fd6446d727b5e311671a78c9af2b50d30d9
Gabriel <gabrielschossler.principal@gmail.com>
2019-01-22 15:19:12 -0200

    teste

---
+++ script.js
@@ -0,0 +1,9 @@
+function chamar(event){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "dados.txt", true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {if (this.readyState === 4 && this.status === 200) {
+        console.log(this.response)}}
+}
+
