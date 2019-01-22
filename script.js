cd0b780b4a9bc3086eb74504454402bf21a5e1b2
Gabriel <gabrielschossler.principal@gmail.com>
2019-01-22 16:24:53 -0200

    teste

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function chamar(event){
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "dados.txt", true);
+    xhttp.open("GET", "dados.txt", true);
     xhttp.send();
 
     xhttp.onreadystatechange = function() {if (this.readyState === 4 && this.status === 200) {
