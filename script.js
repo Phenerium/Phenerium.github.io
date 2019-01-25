7980be1402c3cc6353426439b7fa09987db9bdde
Gabriel <gabrielschossler.principal@gmail.com>
2019-01-25 11:24:42 -0200

    teste

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function entrar(evento){
-    if (evento.key === "Enter") {
-        console.log("foi")
-    }
+function chamar(event){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "dados.xml", true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {if (this.readyState === 4 && this.status === 200) {
+        console.log(this.responseXML)}}
 }
+
